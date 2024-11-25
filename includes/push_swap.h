0f7feed03583d3b6760efc2db0772d49ac9b1a03
Samuel Pérez <samuelperezsantiago12@gmail.com>
2024-11-25 12:39:43 +0100

    Añadir #include al Makefile de push_swap

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,12 +6,15 @@
 /*   By: samperez <samperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 10:49:56 by samperez          #+#    #+#             */
-/*   Updated: 2024/11/25 11:04:57 by samperez         ###   ########.fr       */
+/*   Updated: 2024/11/25 12:38:49 by samperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
 
 typedef struct s_stack
 {
