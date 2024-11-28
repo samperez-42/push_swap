777bdfe776a91141f697385396808216840f35a5
Samuel Pérez <samuelperezsantiago12@gmail.com>
2024-11-28 12:59:57 +0100

    Terminada la funcion push

--- srcs/operations/push.c
+++ srcs/operations/push.c
@@ -6,18 +6,46 @@
 /*   By: samperez <samperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 16:46:06 by samperez          #+#    #+#             */
-/*   Updated: 2024/11/25 16:50:38 by samperez         ###   ########.fr       */
+/*   Updated: 2024/11/28 12:55:34 by samperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void	pa(t_stack_node *stack_a, t_stack_node *stack_b)
+void	push(t_stack_node **dest, t_stack_node **src)
 {
-	
+	t_stack_node	*node_to_push;
+
+	if (!src)
+		return ;
+	node_to_push = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	node_to_push->prev = NULL;
+	if (dest == NULL)
+	{
+		*dest = node_to_push;
+		node_to_push->next = NULL;
+	}
+	else
+	{
+		node_to_push->next = *dest;
+		node_to_push->prev = NULL;
+		*dest = node_to_push;
+	}
 }
 
-void	pb(t_stack_node *stack_b, t_stack_node *stack_a)
+void	pa(t_stack_node *a, t_stack_node *b, bool *checker)
 {
+	push(a, b);
+	if (!checker)
+		write(1, "pa\n", 3);
+}
 
+void	pb(t_stack_node *b, t_stack_node *a, bool *checker)
+{
+	push(b, a);
+	if (!checker)
+		write(1, "pb\n", 3);
 }
