a1130c41fead7e25027ef0b67d837d691d1fdf73
Samuel Pérez <samuelperezsantiago12@gmail.com>
2024-12-04 14:20:09 +0100

    Empiezo chequeo de errores en argv

--- srcs/error_checking.c
+++ srcs/error_checking.c
@@ -6,13 +6,13 @@
 /*   By: samperez <samperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 11:34:16 by samperez          #+#    #+#             */
-/*   Updated: 2024/12/03 12:06:24 by samperez         ###   ########.fr       */
+/*   Updated: 2024/12/04 13:37:30 by samperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-/* void	*free_stack_a(t_stack_node stack_a)
+void	*free_stack(t_stack_node **stack)
 {
 	
 }
@@ -21,4 +21,3 @@ void	check_params(char	*params)
 {
 	
 }
- */
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: samperez <samperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 10:50:00 by samperez          #+#    #+#             */
-/*   Updated: 2024/12/04 13:29:59 by samperez         ###   ########.fr       */
+/*   Updated: 2024/12/04 14:08:26 by samperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ int	main(int argc, char **argv)
 	b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
-	else if (argc == 2) // Array of numbers separated by space case
+	else if (argc == 2)
 		argv = ft_split(argv[1], ' ');
-
+	if (!argv)
+		return (NULL);
 	/*int i = 0;
 	while (argv[i])
 	{
